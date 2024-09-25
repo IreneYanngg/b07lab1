b047c4e33a6c9e08354767122fce0f1762646a29
Irene Yang <ireneyang@Irenes-MacBook-Pro.local>
2024-09-25 13:50:24 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,6 +1,14 @@
 public class Driver {
 	public static void main(String [] args) {
-		Polynomial p = new Polynomial(); System.out.println(p.evaluate(3));
+		
+		double[] co1 = {2, -3.0, 1.0, 2};
+		int[] exp1 = {3, 0, 2, 1};
+		
+		Polynomial p1 = new Polynomial(co1, exp1); 
+		System.out.println("Polynomial p1: " + p1.evaluate(1));
+        System.out.println("Polynomial p1: " + p1.evaluate(0));
+		
+		/*System.out.println(p.evaluate(3));
 		double [] c1 = {6,0,0,5};
 		Polynomial p1 = new Polynomial(c1);
 		double [] c2 = {0,-2,0,0,-9};
@@ -11,5 +19,9 @@ public class Driver {
 			System.out.println("1 is a root of s");
 		else
 			System.out.println("1 is not a root of s");
+		*/
+		
 	}
+	
+	
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,46 +1,128 @@
+import java.util.HashMap;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
 public class Polynomial{
 	// fields
 	double[] coefficients;
+	int[] exponents;
 	
 	// constructor
 	public Polynomial() {
-		coefficients = new double[] {0};
+		this.coefficients = new double[] {0};
+		this.exponents = new int[] {0};
 	}
 	
-	public Polynomial(double[] coeff) {
-		coefficients = new double[coeff.length];
+	public Polynomial(double[] coeff, int[] expo) {
+		this.coefficients = new double[coeff.length];
+		this.exponents = new int[coeff.length];
 		for(int i = 0; i < coeff.length; i++) {
-			coefficients[i] = coeff[i];
+			this.coefficients[i] = coeff[i];
+			this.exponents[i] = expo[i];
+		}
+	}
+	
+	// constructor that takes an argument of type File
+	public Polynomial(File file) throws FileNotFoundException {
+		ArrayList<Double> co_list = new ArrayList<>(); // '<>()'中的'<>'菱形操作符可以用来避免重复指定数据类型
+		ArrayList<Integer> exp_list = new ArrayList<>(); // 左边指定为type double了右边就不用再指定一次了
+		
+		Scanner input_file = new Scanner(file);
+		String poly_string = input_file.nextLine();
+		
+		get_values(poly_string, co_list, exp_list);
+		
+		int result_len = co_list.size();
+		coefficients = new double[result_len];
+		exponents = new int[result_len];
+		for(int i = 0; i < result_len; i++) {
+			coefficients[i] = co_list.get(i);
+			exponents[i] = exp_list.get(i);
+		}
+		
+		input_file.close();
+		
+	}
+	
+	// constructor helper
+	private void get_values(String poly, ArrayList<Double> co_list, ArrayList<Integer> exp_list) {
+		String[] terms = poly.split("(?=[+-])");
+		
+		for(String term : terms) {
+			String[] parts = term.split("x");
+			double cur_coeff = 1.0;
+			int cur_exp = 0;
+			
+			if(parts.length == 0)
+				continue; // skil invalid parts
+			else if(parts.length == 1)
+				cur_coeff = Double.parseDouble(parts[0]); // only number is coeff, exp is default value 0
+			else {
+				// cur_coeff
+				if(parts[0].equals("") || parts[0].equals("+"))
+					continue; // 'x' or '+x': coeff=1.0
+				else if(parts[0].equals("-"))
+					cur_coeff = -1.0; // '-x': coeff=-1.0
+				else
+					cur_coeff = Double.parseDouble(parts[0]);
+				
+				// cur_exp
+				if(parts[1].equals(""))
+					continue; // exp is default value 0
+				else
+					cur_exp = Integer.parseInt(parts[1]);
+			}
+			
+			co_list.add(cur_coeff);
+			exp_list.add(cur_exp);	
 		}
 	}
 	
 	
 	public Polynomial add(Polynomial to_add) {
-		int new_len = Math.max(coefficients.length, to_add.coefficients.length);
+		int new_len = Math.max(this.coefficients.length, to_add.coefficients.length);
 		double[] new_co = new double[new_len];
+		int[] new_exp = new int[new_len];		
 		
-		for (int i = 0; i < new_len; i++) {
-			double a = 0;
-			if (i < coefficients.length) {
-				a = coefficients[i];
-			}
-			double b = 0;
-			if (i < to_add.coefficients.length) {
-				b = to_add.coefficients[i];
+		if(this.coefficients.length >= to_add.coefficients.length) {
+			for(int i = 0; i < this.coefficients.length; i++) {
+				new_co[i] = this.coefficients[i];
+				new_exp[i] = this.exponents[i];
+				for(int j = 0; j < to_add.coefficients.length; j++) {
+					if (to_add.exponents[j] == this.exponents[i]) {
+						new_co[i] += to_add.coefficients[j];
+					}
+				}
+		
+				
 			}
 			
-			new_co[i] = a + b;
+		} else {
+			
+			for(int i = 0; i < to_add.coefficients.length; i++) {
+				new_co[i] = to_add.coefficients[i];
+				new_exp[i] = to_add.exponents[i];
+				for(int j = 0; j < this.coefficients.length; j++) {
+					new_co[i] += this.coefficients[j];
+				}
+			}
+			
 		}
 		
-		Polynomial new_poly = new Polynomial(new_co);
+		Polynomial new_poly = new Polynomial(new_co, new_exp);
 		
 		return new_poly;
 	}
 	
 	public double evaluate(double x) {
 		double result = 0;
-		for(int i = 0; i < coefficients.length; i++) {
-			result = result + coefficients[i] * Math.pow(x,i);
+		for(int i = 0; i < this.coefficients.length; i++) {
+			result = result + this.coefficients[i] * Math.pow(x,exponents[i]);
 		}
 		
 		return result;
@@ -48,18 +130,108 @@ public class Polynomial{
 	}
 	
 	public boolean hasRoot(double val) {
-		if(coefficients.length < 1) {
-			return false;
+		return (evaluate(val) == 0);
+	}
+	
+	public Polynomial multiply(Polynomial to_multi) {
+		
+		/*int pivot_len = (this.coefficients.length * to_multi.coefficients.length);
+		pivot_co = new double[pivot_len];
+		pivot_exp = new int[pivot_len];
+		int index = 0;
+		
+		for (int i = 0; i < this.coefficients.length; i++) {
+			for(int j = 0; j < to_multi.coefficients.length; j++) {
+				pivot_co[index] = this.coefficients[i]*to_multi.coefficients[j];
+				pivot_exp[index] = this.exponents[i]+to_multi.exponents[j];
+				index += 1;
+			}
 		}
 		
-		double result = 0;
-		for(int i = 0; i < coefficients.length; i++) {
-			result = result + coefficients[i] * Math.pow(val,i);
+		HashSet<Integer> distinct_exp = new HashSet<>();
+		for(int i = 0; i < pivot_len; i++) {
+			distinct_exp.add(pivot_exp[i]);
 		}
 		
-		return (result == 0);
+		result_len = distinct_exp.size();
+		result_co = new double[result_len] {0};
+		result_exp = new int[result_len];
+		Iterator<Integer> iterator = distinct_exp.iterator();
+		for(int i = 0; i < result_len; i++) {
+			result_exp[i] = iterator.next();
+		}
+		
+		for (int i = 0; i < pivot_len; i++) {
+			for(int j = 0; j < result_len; j++) {
+				if(result_exp[j] == pivot_exp[i]) result_co[j] += pivot_co[i];
+			}
+		}*/
+		
+		HashMap<Integer, Double> result_map = new HashMap<>();
+		
+		for(int i = 0; i < this.coefficients.length; i++) {
+			for(int j = 0; j < to_multi.coefficients.length; j++) {
+				
+				double new_co = this.coefficients[i] * to_multi.coefficients[j];
+				int new_exp = this.exponents[i] + to_multi.exponents[j];
+				
+				result_map.put(new_exp, result_map.getOrDefault(new_exp, 0.0) + new_co);
+					
+			}
+		}
+		
+		int result_len = result_map.size();
+		double[] result_co = new double[result_len];
+		int[] result_exp = new int[result_len];
+		
+		Integer[] keys = result_map.keySet().toArray(new Integer[0]);
+		for(int i = 0; i < result_len; i++) {
+			int cur_exp = keys[i];
+			result_co[i] = result_map.get(cur_exp);
+			result_exp[i] = cur_exp;
+		}
+		
+		Polynomial result = new Polynomial(result_co, result_exp);
+		return result;
+		
+	}
+	
+	public void saveToFile(String file_to_write) throws IOException {
+		
+		FileWriter fileWriter = new FileWriter(file_to_write);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+		
+		StringBuilder poly_str = new StringBuilder();
+		
+		for(int i = 0; i < this.coefficients.length; i++) {
+			
+			double cur_coeff = this.coefficients[i];
+			int cur_exp = this.exponents[i];
+			
+			if(cur_coeff < 0) // cur_coeff < 0
+				poly_str.append(cur_coeff);
+			else { // cur_coeff > 0
+				if(i == 0) poly_str.append(cur_coeff);
+				else poly_str.append("+").append(cur_coeff);
+			}
+			
+			if(cur_exp == 1)
+				poly_str.append("x");
+			else if(cur_exp > 1)
+				poly_str.append("x").append(cur_exp);
+			
+		}
+		
+		printWriter.println(poly_str.toString());
+		
+		printWriter.close();
+        fileWriter.close();
+		
+		
 	}
 	
 	
 }
+
+
 
